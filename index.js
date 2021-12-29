eed964d29ec4cfdea8c64e1391fc66f2db6115c3
PrinceCodes <princecodes247@gmail.com>
2021-12-29 23:45:23 +0100

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+// #!/usr/bin/env node
+
+// const chalk = require('chalk');
+// const clear = require('clear');
+// const figlet = require('figlet');
+
+const files = require('./utils/files');
+
+
+// clear();
+
+// console.log(
+//   chalk.yellow(
+//     figlet.textSync('Alfonso', { horizontalLayout: 'full' })
+//   )
+// );
+
+// if (files.directoryExists('.git')) {
+//   console.log(chalk.red('Already exists'));
+//   process.exit();
+// }
+
+
+const run =  () => {
+ 
+};
+
+run();
